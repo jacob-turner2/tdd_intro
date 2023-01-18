02be632c252fce0cd0e4ed0002128b57c8e77f83
jacob-turner2 <97054771+jacob-turner2@users.noreply.github.com>
2023-01-17 19:10:15 -0500

    Password Checker V0.1 - 2nd Test

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Password Checker</h1>
+
+<h3>TDD-1 test project</h3>
+
+This is a simple demonstration that uses ScalaTest in a Scala3
+project. The program checks the sufficiency of a password provided by a user against various
+rules to ensure good complexity.
---
+++ src/test/scala/password_checker/Test.scala
@@ -0,0 +1,25 @@
+package password_checker
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should._
+
+class Password_Test extends AnyFunSpec with Matchers{
+  describe("A password checker") {
+    describe("determines proper length"){
+        it("rejects passwords that are less than 6 characters"){
+            val tooShort = "abcde"
+            val longEnough = "abcdef"
+            val evenLonger = "abcdefghi"
+            Password.isValid(tooShort) shouldBe false
+            Password.isValid(longEnough) shouldBe true
+            Password.isValid(evenLonger) shouldBe true
+        }
+        it("rejects passwords without a number in it"){
+          val nonumber = "abcdef"
+          val hasnumber = "abcdef6"
+          Password.isValid(nonumber) shouldBe false
+          Password.isValid(hasnumber) shouldBe true
+        }
+    }
+  }
+}
